29ad386c87084b87a8368eb65eaac691269f5d0d
Akshat Sipany <borncancer21@gmail.com>
2025-04-12 05:48:48 +0530

    checking cors issue

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'https://shelf-frontend.vercel.app/',
+    origin: "*",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
